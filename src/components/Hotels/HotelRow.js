6b43b36e33cee8ce3dab9e469185f06ccc6e0cce
Jack Howard <jackhowaa@gmail.com>
2019-04-10 19:12:30 -0500

    Refactor row style and containers out of app.js

--- src/components/Hotels/HotelRow.js
+++ src/components/Hotels/HotelRow.js
@@ -1,19 +1,32 @@
 import React from 'react';
 import PropTypes from 'prop-types';
+import FallbackHotelImage from '../../public/images/photo-error.png';
+import ImageContainer from '../App/ImageContainer';
+import CommonString from '../../common/CommonString';
 
 const HotelRow = ({ data }) => (
-  <div>
-    <img src={data.hotelStaticContent.mainImage.url} className="photo" />
-    <div className="details">
-      <div>{data.hotelStaticContent.name}</div>
-      <div>{data.hotelStaticContent.neighborhoodName}</div>
+  <div className="hotel-list-element">
+    <ImageContainer
+      imageURL={data.hotelStaticContent.mainImage.url}
+      fallbackImage={FallbackHotelImage}
+      alt="Hotel available to stay at"
+    />
+    <div className="label-container">
+      <h1>{data.hotelStaticContent.name}</h1>
+      <p className="label">{data.hotelStaticContent.neighborhoodName}</p>
     </div>
-    <div className="price">
-      {data.lowestAveragePrice.symbol}
-      {data.lowestAveragePrice.amount}
-      {data.rewards.miles}
-      <button>Select</button>
+    <div className="label-container">
+      <p className="amount">
+        {CommonString.convertAsciiToSymbol(data.lowestAveragePrice.symbol)}
+        {data.lowestAveragePrice.amount}
+      </p>
+      <p className="label">Price</p>
     </div>
+    <div className="label-container">
+      <p className="amount">{data.rewards.miles}</p>
+      <p className="label">Miles</p>
+    </div>
+    <button type="button">Select</button>
   </div>
 );
 
--- src/components/Hotels/HotelsContainer.js
+++ src/components/Hotels/HotelsContainer.js
@@ -1,12 +1,52 @@
-import React from 'react';
+import React, { Component } from 'react';
+import PropTypes from 'prop-types';
+
 import HotelRow from './HotelRow';
 
-const HotelsContainer = ({ hotels }) => (
-  <div className="hotel-list">
-    {hotels.map(hotelData => (
-      <HotelRow key={hotelData.id} data={hotelData} />
-    ))}
-  </div>
-);
+class HotelsContainer extends Component {
+  filterHotelRows = () => {
+    const { hotels, hotelNameFilter } = this.props;
+
+    const lowerHotelNameFilter = hotelNameFilter.toLowerCase();
+    return hotels.filter(hotelObject =>
+      hotelObject.hotelStaticContent.name
+        .toLowerCase()
+        .includes(lowerHotelNameFilter)
+    );
+  };
+
+  sortRowsByPrice = hotels =>
+    hotels.sort(
+      (a, b) => a.lowestAveragePrice.amount - b.lowestAveragePrice.amount
+    );
+
+  rowDisplayHandler = () => {
+    const filteredRows = this.filterHotelRows();
+    const { ascendingSort } = this.props;
+
+    if (ascendingSort) {
+      return this.sortRowsByPrice(filteredRows);
+    }
+
+    return filteredRows;
+  };
+
+  render() {
+    const filteredHotels = this.rowDisplayHandler();
+    return (
+      <div className="hotel-list">
+        {filteredHotels.map(hotelData => (
+          <HotelRow key={hotelData.id} data={hotelData} />
+        ))}
+      </div>
+    );
+  }
+}
+
+HotelsContainer.propTypes = {
+  hotels: PropTypes.instanceOf(Array).isRequired,
+  hotelNameFilter: PropTypes.string.isRequired,
+  ascendingSort: PropTypes.bool.isRequired
+};
 
 export default HotelsContainer;
